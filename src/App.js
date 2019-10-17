3dfbd99fb5d17610fe3b2c81ed5492159593bb0c
rishabh verma <vermarishabh0987@gmail.com>
2019-10-17 23:17:12 +0530

    basic implmentation of silder done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
-  );
+//NOTE:DO NOT NAME CLASS AS CONATINER...
+
+class App extends Component {
+  state = {
+    slide: false
+  };
+
+  style = slide => {
+    var x = slide ? "-200px" : "0px";
+    return {
+      transform: "translateX(" + x + ")",
+      transition: "transform 0.5s ease-out"
+    };
+  };
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-2 sideMenu" style={this.style(this.state.slide)}>
+            <button
+              onClick={() => this.setState({ slide: !this.state.slide })}
+              style={{ marginLeft: "200px" }}
+            >
+              ok
+            </button>
+          </div>
+          <div className="col-10 main"></div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
