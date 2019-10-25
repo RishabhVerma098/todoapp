e04250ebf7e665e9d49100823d711745b4ca6514
rishabh verma <vermarishabh0987@gmail.com>
2019-10-25 12:12:47 +0530

    changes made to card

--- src/components/reusable/card/card.js
+++ src/components/reusable/card/card.js
@@ -6,31 +6,29 @@ class CardComponent extends Component {
     return (
       <div className="card-container">
         <div className="card-row-title">
-          <input className="title" type="text" placeholder="Title"></input>
+          <span className="title">Title</span>
           <div className="tick" onClick={this.props.changeSize}>
             <i class="fas fa-check"></i>
           </div>
         </div>
         <hr style={{ marginTop: "2px" }}></hr>
         <div className="card-row-description">
-          <textarea
-            rows="4"
-            cols="50"
-            className="description"
-            placeholder="Description"
-          ></textarea>
+          <p className="description">
+            Occaecat culpa officia et cupidatat. Cillum veniam ut tempor
+            ullamco...
+          </p>
         </div>
         <div className="fotter-controls">
           <div className="labrem">
             <div className="label">
-              <i class="fas fa-tags"></i>
+              <i class="fas fa-tags" style={{ color: "#8DA2DD" }}></i>
             </div>
             <div className="reminder">
-              <i class="fas fa-bell"></i>
+              <i class="fas fa-bell" style={{ color: "#8DDDA5" }}></i>
             </div>
           </div>
           <div className="expandCard">
-            <i class="fas fa-expand"></i>
+            <i class="fas fa-expand" style={{ color: "#8D8686" }}></i>
           </div>
         </div>
       </div>
